996dedd926e9a308f0a3ec7368eeadb67a825a44
Barbora Ulipova <b.ulipova@gmail.com>
2014-04-22 19:06:01 +0200

    1. pokus

---
+++ pexeso.py
@@ -0,0 +1,28 @@
+﻿import pprint
+
+slova = []
+
+with open ('slova.txt') as soubor:
+	for radek in soubor:
+		slova.append(radek.split())
+		
+pprint.pprint(slova)
+
+def slovo_podle_indexu(cislo, jazyk):
+	if jazyk == "cesky":
+		index = 0
+	elif jazyk == "anglicky":
+		index = 1
+	else:
+		raise ValueError(jazyk) # resi, kdyz se zada spatny jazyk
+	return (slova[cislo][index])
+	
+print (slovo_podle_indexu(3,"cesky"))
+
+def test_slovo_podle_indexu():
+	assert slovo_podle_indexu(1,"cesky") == "Kolečko" # assert zkontroluje, jestli je to v zavorce true. Kdyz napisu py.test "nazev programu" zkuntroluje asserty u funkci, ktere zacinaji test
+	assert slovo_podle_indexu(2,"anglicky") == "Smile"
+
+
+		
+	
