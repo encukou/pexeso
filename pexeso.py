0f2c14ab174e0a89172c932ab1c28485153feede
Petr Viktorin <pviktori@redhat.com>
2014-04-22 19:53:13 +0200

    Funkce vyber_kartu

--- pexeso.py
+++ pexeso.py
@@ -17,8 +17,24 @@ def slovo_podle_indexu(cislo, jazyk):
     else:
         raise ValueError(jazyk)
 
+def vyber_kartu(stav, cislo_radku, cislo_sloupce):
+    radek = stav[cislo_radku]
+    prvek = radek[cislo_sloupce]
+    return prvek
+
 
 def test_slovo_podle_indexu():
     assert slovo_podle_indexu(1, 'C') == 'Kolečko'
-    assert (slovo_podle_indexu(2, 'A') ==
-            'Smile')
+    assert slovo_podle_indexu(2, 'A') == 'Smile'
+
+def test_vyberu_karty():
+    stav = [
+        [(0, 'C'), (0, 'A'), (1, 'C'), (1, 'A')],
+        [(2, 'C'), (2, 'A'), (3, 'C'), (3, 'A')],
+        [(4, 'C'), (4, 'A'), (5, 'C'), (5, 'A')],
+        [(6, 'C'), (6, 'A'), (7, 'C'), (7, 'A')],
+    ]
+    assert vyber_kartu(stav, 0, 0) == (0, 'C')
+    assert vyber_kartu(stav, 0, 1) == (0, 'A')
+    assert vyber_kartu(stav, 2, 3) == (5, 'A')
+    assert vyber_kartu(stav, 3, 3) == (7, 'A')
