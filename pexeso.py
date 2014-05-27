4835eb06851c17b0ae568296c109071b9526a53f
Barbora Ulipova <b.ulipova@gmail.com>
2014-05-27 20:33:44 +0200

    udelana funkce, ktera udela tah

--- pexeso.py
+++ pexeso.py
@@ -28,11 +28,10 @@ def slovo_podle_indexu(cislo, jazyk):
 		raise ValueError(jazyk) # resi, kdyz se zada spatny jazyk
 	return (slova[cislo][index])
 	
-print (slovo_podle_indexu(3,"cesky"))
 
 
-def vyber_kartu(seznam,radek,sloupec): # kdyz zavolam s argumentem(stav,0,0), vrati (0,"cesky")
-		return seznam[radek][sloupec]
+def vyber_kartu(stav,radek,sloupec): # kdyz zavolam s argumentem(stav,0,0), vrati (0,"cesky")
+		return stav[radek][sloupec]
 		
 
 
@@ -65,7 +64,37 @@ def vypis_stav(seznam_karet):
 		print ()
 vypis_stav(zamichej_karty())	
 
+"""zakladni_stav = [
+    [(0, 'C', False), (0, 'A', False), (1, 'C', False), (1, 'A', False)],
+    [(2, 'C', False), (2, 'A', False), (3, 'C', False), (3, 'A', False)],
+    [(4, 'C', False), (4, 'A', False), (5, 'C', False), (5, 'A', False)],
+    [(6, 'C', False), (6, 'A', False), (7, 'C', False), (7, 'A', False)],
+]
 
+zakladni_hra = {
+    'stav': zakladni_stav,
+    'aktivni_karta': None,
+}"""
+
+def otoc_kartu(stav,radek,sloupec,nove_otoceni):
+	""" sem se maji napsat komentare k funkci """ 
+	cislo, jazyk, stare_otoceni = stav[radek][sloupec] # stav[radek][sloupec] vrati trojici, tu tuple, ve ktere mam cislo, jazyk, otoceni
+	stav[radek][sloupec] = cislo, jazyk, nove_otoceni
+
+def udelej_tah(hra, radek, sloupec):
+	if  hra['aktivni_karta'] == None:
+		otoc_kartu(hra['stav'],radek,sloupec,True)
+		hra['aktivni_karta'] = (radek,sloupec)
+	else:
+		aktivni_radek, aktivni_sloupec = hra['aktivni_karta']
+		nove_otoceny_obrazek = vyber_kartu(hra['stav'],radek,sloupec)[0]
+		puvodni_obrazek = vyber_kartu(hra['stav'],aktivni_radek,aktivni_sloupec)[0]
+		if nove_otoceny_obrazek == puvodni_obrazek:
+			otoc_kartu(hra['stav'],radek,sloupec,True)
+		else:
+			otoc_kartu(hra['stav'],aktivni_radek,aktivni_sloupec,False)
+		hra['aktivni_karta'] = None
+	return hra
 
 # funkce vs. metoda: metodu volam na objektu = napisu seznam.append, u funkce vkladam objekt jako argument - pisu shuffle(seznam_karet)	
 		
