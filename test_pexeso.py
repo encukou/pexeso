eccd15a39e71ae27a587de9f17e3c0e6e128d115
Barbora Ulipova <b.ulipova@gmail.com>
2014-06-03 17:56:45 +0200

    pridana funkce, ktera vzdy vytvori ten samy zakladni stav

--- test_pexeso.py
+++ test_pexeso.py
@@ -32,3 +32,66 @@ def test_zamichani():
 
 def test_nahodneho_zamichani():
 	assert pexeso.zamichej_karty() != pexeso.zamichej_karty()
+	
+def vytvor_zakladni_stav():
+	zakladni_stav = [
+    [(0, 'C', False), (0, 'A', False), (1, 'C', False), (1, 'A', False)],
+    [(2, 'C', False), (2, 'A', False), (3, 'C', False), (3, 'A', False)],
+    [(4, 'C', False), (4, 'A', False), (5, 'C', False), (5, 'A', False)],
+    [(6, 'C', False), (6, 'A', False), (7, 'C', False), (7, 'A', False)],
+	]
+	return zakladni_stav
+
+def vytvor_zakladni_hru():
+	zakladni_stav = vytvor_zakladni_stav()
+	zakladni_hra = {
+    'stav': zakladni_stav,
+    'aktivni_karta': None,
+	}
+	return zakladni_hra
+
+def zkontroluj_otoceni(stav, otocene_karty):
+    for radek in range(4):
+        for sloupec in range(4):
+            prvek = pexeso.vyber_kartu(stav, radek, sloupec)
+            if (radek, sloupec) in otocene_karty:
+                assert prvek[2] == True
+            else:
+                assert prvek[2] == False
+
+def test_hry2():
+    hra = vytvor_zakladni_hru()
+    hra = pexeso.udelej_tah(hra, 1, 2)
+    zkontroluj_otoceni(hra['stav'], [(1, 2)])
+    assert hra['aktivni_karta'] == (1, 2)
+
+    hra = pexeso.udelej_tah(hra, 0, 0)
+    zkontroluj_otoceni(hra['stav'], [])
+    assert hra['aktivni_karta'] == None
+
+    hra = pexeso.udelej_tah(hra, 1, 2)
+    zkontroluj_otoceni(hra['stav'], [(1, 2)])
+    assert hra['aktivni_karta'] == (1, 2)
+
+    hra = pexeso.udelej_tah(hra, 1, 3)
+    zkontroluj_otoceni(hra['stav'], [(1, 2), (1, 3)])
+    assert hra['aktivni_karta'] == None
+				
+
+def test_hry():
+    hra = vytvor_zakladni_hru()
+    hra = pexeso.udelej_tah(hra, 1, 2)
+    zkontroluj_otoceni(hra['stav'], [(1, 2)])
+    assert hra['aktivni_karta'] == (1, 2)
+
+    hra = pexeso.udelej_tah(hra, 0, 0)
+    zkontroluj_otoceni(hra['stav'], [])
+    assert hra['aktivni_karta'] == None
+
+    hra = pexeso.udelej_tah(hra, 1, 2)
+    zkontroluj_otoceni(hra['stav'], [(1, 2)])
+    assert hra['aktivni_karta'] == (1, 2)
+
+    hra = pexeso.udelej_tah(hra, 1, 3)
+    zkontroluj_otoceni(hra['stav'], [(1, 2), (1, 3)])
+    assert hra['aktivni_karta'] == None
